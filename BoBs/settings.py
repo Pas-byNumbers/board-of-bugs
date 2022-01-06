ea43ee13bc84336ee6fc4b5d7e289a3bd0095515
Pas-byNumbers <pasbynumbers@gmail.com>
2022-01-06 22:22:47 +0000

    post-react pt2

--- BoBs/settings.py
+++ BoBs/settings.py
@@ -128,16 +128,17 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
+    os.path.join(BASE_DIR, "build/static"),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'client', 'build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
